182dd65b3cf06e6299afd9eba39ff0fecd39e1af
Ilia Munaev <ilyamunaev@gmail.com>
2024-10-31 14:49:46 +0200

    Feat: update libft.h

--- libft.h
+++ libft.h
@@ -1,15 +1,24 @@
 #ifndef LIBFT_H
 #define LIBFT_H
 
+#include <string.h>
+
 
 /*
  * Character Classification and Conversion Functions
 */
 int	ft_isalpha(int ch);
 int	ft_isprint(int c);
+int	ft_tolower(int c);
+int	ft_toupper(int c);
+int	ft_isdigit(int c);
+int	ft_isascii(int ch);
+size_t	ft_strlen(const char *s);
 
 /*
  * String Manipulation Functions
 */
+size_t	ft_strlen(const char *s);
+size_t	ft_strlcat(char *dest, const char *src, size_t size);
 
 #endif //LIBFT_H

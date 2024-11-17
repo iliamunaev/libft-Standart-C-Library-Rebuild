3795b1cfa6a109d8479c7d616aa9d634545ce2bc
Ilia Munaev <ilyamunaev@gmail.com>
2024-11-17 22:16:02 +0200

    Fix: add additional protection

--- ft_putendl_fd.c
+++ ft_putendl_fd.c
@@ -6,7 +6,7 @@
 /*   By: imunaev- <imunaev-@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/07 09:28:23 by imunaev-          #+#    #+#             */
-/*   Updated: 2024/11/07 09:28:28 by imunaev-         ###   ########.fr       */
+/*   Updated: 2024/11/17 22:12:18 by imunaev-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,8 @@
 
 void	ft_putendl_fd(char *s, int fd)
 {
+	if (!s)
+		return ;
 	ft_putstr_fd(s, fd);
 	ft_putchar_fd('\n', fd);
 }

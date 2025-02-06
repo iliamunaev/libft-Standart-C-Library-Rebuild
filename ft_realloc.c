050bd43078393749e86067327d72a685593a3bf8
Ilia Munaev <ilyamunaev@gmail.com>
2025-02-06 21:53:15 +0200

    Refactor: ft_realloc according to the School 42 code style

--- ft_realloc.c
+++ ft_realloc.c
@@ -6,7 +6,7 @@
 /*   By: imunaev- <imunaev-@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/06 21:40:49 by imunaev-          #+#    #+#             */
-/*   Updated: 2025/02/06 21:44:30 by imunaev-         ###   ########.fr       */
+/*   Updated: 2025/02/06 21:52:22 by imunaev-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,18 +35,18 @@ void	*ft_realloc(void *ptr, size_t old_size, size_t new_size)
 	size_t	bytes_to_copy;
 
 	if (!ptr)
-		return malloc(new_size);
+		return (malloc(new_size));
 	if (new_size == 0)
 	{
 		free(ptr);
-		return NULL;
+		return (NULL);
 	}
 	if (new_size == old_size)
 		return (ptr);
 	new_ptr = malloc(new_size);
 	if (!new_ptr)
 		return (NULL);
-	if(old_size < new_size)
+	if (old_size < new_size)
 		bytes_to_copy = old_size;
 	else
 		bytes_to_copy = new_size;
